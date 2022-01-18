a5d7b270939beba90baf542205666cc59d92d77f
Aaron Alef <aaron.alef@code.berlin>
2022-01-18 15:04:12 +0100

    feat(multiply): fix x2 enlargement algorithm for 3-channel images

--- lib/multiply.hpp
+++ lib/multiply.hpp
@@ -12,11 +12,64 @@ cv::Mat twice (cv::Mat const & original) {
     cv::Mat img (2 * original.rows, 2 * original.cols, CV_8UC3);
     for (int y = 0; y < img.rows; y++) {
         auto original_row = original.ptr <Color_BGR> (y / 2);
-        auto img_row = img.ptr <double> (y);
+        auto img_row = img.ptr <Color_BGR> (y);
 
         for (int x = 0; x < img.cols; x++) {
-            Color_BGR color = original_row [x / 2];
-            img_row [x] = (double) (color.red() + color.green() + color.blue()) / 3;
+            int ctr = 1;
+            unsigned int red = original_row [x / 2].red();
+            unsigned int green = original_row [x / 2].green();
+            unsigned int blue = original_row [x / 2].blue();
+            if ((x-1)/2 > 0) {
+                ctr++;
+                red += original.at <Color_BGR> (y/2, (x-1)/2).red();
+                green += original.at <Color_BGR> (y/2, (x-1)/2).green();
+                blue += original.at <Color_BGR> (y/2, (x-1)/2).blue();
+            }
+            if ((y-1)/2 > 0) {
+                ctr++;
+                red += original.at <Color_BGR> ((y-1)/2, x/2).red();
+                green += original.at <Color_BGR> ((y-1)/2, x/2).green();
+                blue += original.at <Color_BGR> ((y-1)/2, x/2).blue();
+            }
+            if ((x+1)/2 < original.rows) {
+                ctr++;
+                red += original.at <Color_BGR> (y/2, (x+1)/2).red();
+                green += original.at <Color_BGR> (y/2, (x+1)/2).green();
+                blue += original.at <Color_BGR> (y/2, (x+1)/2).blue();
+            }
+            if ((y+1)/2 < original.cols) {
+                ctr++;
+                red += original.at <Color_BGR> ((y+1)/2, x/2).red();
+                green += original.at <Color_BGR> ((y+1)/2, x/2).green();
+                blue += original.at <Color_BGR> ((y+1)/2, x/2).blue();
+            }
+            if ((x-1)/2 > 0 && (y-1)/2 > 0) {
+                ctr++;
+                red += original.at <Color_BGR> ((y-1)/2, (x-1)/2).red();
+                green += original.at <Color_BGR> ((y-1)/2, (x-1)/2).green();
+                blue += original.at <Color_BGR> ((y-1)/2, (x-1)/2).blue();
+            }
+            if ((x-1)/2 > 0 && (y+1)/2 < original.cols) {
+                ctr++;
+                red += original.at <Color_BGR> ((y-1)/2, (x+1)/2).red();
+                green += original.at <Color_BGR> ((y-1)/2, (x+1)/2).green();
+                blue += original.at <Color_BGR> ((y-1)/2, (x+1)/2).blue();
+            }
+            if ((x+1)/2 < original.rows && (y-1)/2 > 0) {
+                ctr++;
+                red += original.at <Color_BGR> ((y+1)/2, (x-1)/2).red();
+                green += original.at <Color_BGR> ((y+1)/2, (x-1)/2).green();
+                blue += original.at <Color_BGR> ((y+1)/2, (x-1)/2).blue();
+            }
+            if ((x+1)/2 < original.rows && (y+1)/2 < original.cols) {
+                ctr++;
+                red += original.at <Color_BGR> ((y+1)/2, (x+1)/2).red();
+                green += original.at <Color_BGR> ((y+1)/2, (x+1)/2).green();
+                blue += original.at <Color_BGR> ((y+1)/2, (x+1)/2).blue();
+            }
+            img_row [x].red() = red / ctr;
+            img_row [x].green() = green / ctr;
+            img_row [x].blue() = blue / ctr;
         }
     }
     return img;
