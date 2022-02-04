633c3a9e068ec5dd9bfc949e68a0fba750df7c48
Aaron Alef <aaron.alef@code.berlin>
2022-02-04 12:37:50 +0100

    feat(scale): implement coloured scaling

--- lib/spatial.cpp
+++ lib/spatial.cpp
@@ -30,44 +30,25 @@ cv::Mat scale (cv::Mat const & src, unsigned char min, unsigned char max) {
     auto dest = src.clone();
 
     unsigned char _min = 255;
-    for (int y = 0; y < src.rows; y++) {
-        auto row = src.ptr <Color_BGR> (y);
-
-        for (int x = 0; x < src.cols; x++) {
-            for (auto const color : row [x].value) {
-                _min = std::min (color, _min);
-            }
-        }
-    }
-
-    for (int y = 0; y < src.rows; y++) {
-        auto drow = dest.ptr <Color_BGR> (y);
-
-        for (int x = 0; x < src.cols; x++) {
-            for (auto & color : drow [x].value) {
-                color += min - _min;
-            }
-        }
-    }
-
     unsigned char _max = 0;
     for (int y = 0; y < src.rows; y++) {
         auto row = src.ptr <Color_BGR> (y);
 
         for (int x = 0; x < src.cols; x++) {
             for (auto const color : row [x].value) {
+                _min = std::min (color, _min);
                 _max = std::max (color, _max);
             }
         }
     }
 
-    double factor = double (max) / _max;
+    double factor = double (max) / (_max - _min + min);
     for (int y = 0; y < src.rows; y++) {
-        auto drow = dest.ptr <Color_BGR> (y);
+        auto row = dest.ptr <Color_BGR> (y);
 
         for (int x = 0; x < src.cols; x++) {
-            for (auto & color : drow [x].value) {
-                color *= factor;
+            for (auto & color : row [x].value) {
+                color = factor * (color + min - _min);
             }
         }
     }
@@ -79,37 +60,22 @@ cv::Mat scale_gray (cv::Mat const & src, unsigned char min, unsigned char max) {
     auto dest = src.clone();
 
     double _min = 255;
-    for (int y = 0; y < src.rows; y++) {
-        auto row = src.ptr <double> (y);
-
-        for (int x = 0; x < src.cols; x++) {
-            _min = std::min (row [x], _min);
-        }
-    }
-
-    for (int y = 0; y < src.rows; y++) {
-        auto drow = dest.ptr <double> (y);
-
-        for (int x = 0; x < src.cols; x++) {
-            drow [x] += min - _min;
-        }
-    }
-
     double _max = 0;
     for (int y = 0; y < src.rows; y++) {
         auto row = src.ptr <double> (y);
 
         for (int x = 0; x < src.cols; x++) {
+            _min = std::min (row [x], _min);
             _max = std::max (row [x], _max);
         }
     }
 
-    double factor = double (max) / _max;
+    double factor = double (max) / (_max - _min + min);
     for (int y = 0; y < src.rows; y++) {
         auto drow = dest.ptr <double> (y);
 
         for (int x = 0; x < src.cols; x++) {
-            drow [x] *= factor;
+            drow [x] = factor * (drow [x] + min - _min);
         }
     }
 
